9289df4d7577d2c75ad28fd6fe8b5589468a830f
Maik <164011129+Maikl75@users.noreply.github.com>
2024-11-13 17:39:33 +0100

    Update streamlit_app.py
    text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,9 @@ import streamlit as st
 
 st.title("🎈 Maik new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."    
 )
+st.title("🎈 Chatbot")
 import requests
 
 API_URL = "https://qa-admin.precisionai-portal.bayer.com:8510/api/v1/prediction/05d59e9b-376a-41be-bff7-764baf8737a8"
